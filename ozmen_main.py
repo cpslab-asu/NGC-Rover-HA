de239febd767d7fb5de95d5766f49ae3177ceef2
Tanmay Khandait (Student) <tkhandai@en4230638l.fulton.ad.asu.edu>
2024-09-13 02:04:20 -0700

    Show falsification

--- ozmen_main.py
+++ ozmen_main.py
@@ -1,3 +1,4 @@
+from copy import copy
 from typing import Any, List
 import numpy as np
 from dataclasses import dataclass
@@ -18,9 +19,10 @@ import matplotlib.pyplot as plt
 
 
 class Movement:
-    def __init__(self):
-        self.dt_integral = 0.01
-        self.delta_t = 1
+    def __init__(self, delta_t = 0.1):
+        self.delta_t = delta_t
+        self.dt_integral = self.delta_t/10
+        
         self.history_t = [0.0]
         self.history_x = [0.0]
         self.history_y = [0.0]
@@ -30,17 +32,17 @@ class Movement:
         x = self.history_x[-1]
         y = self.history_y[-1]
         theta = self.history_theta[-1]
-
+        
         for t in np.arange(0, 1, self.dt_integral):
             y += v * np.sin(theta) * self.dt_integral
             x += v * np.cos(theta) * self.dt_integral
             theta += omega * self.dt_integral
             
             # Store the positions
-            self.history_x.append(x)
-            self.history_y.append(y)
-            self.history_theta.append(theta)
-            self.history_t.append(self.history_t[-1]+self.dt_integral)
+        self.history_x.append(x)
+        self.history_y.append(y)
+        self.history_theta.append(theta)
+        self.history_t.append(self.history_t[-1]+self.delta_t)
     
     def getCompass(self):
         return self.history_theta[-1]
@@ -62,6 +64,8 @@ class Flags:
     updateCompass: bool = False
     continueMoving: bool = False
     manualActions:ManualActions = ManualActions()
+    pastFlag_s3_history: int = 0
+    history= [0]
 
     def print_state(self):
         print("****************************************")
@@ -105,6 +109,9 @@ class Flags:
     
     def set_ManualActions(self, manualActions: ManualActions):
         self.manualActions = manualActions
+
+    def set_pastFlag_s3_history(self, pastFlag_s3_history: int):
+        self.pastFlag_s3_history = pastFlag_s3_history
 
 @dataclass
 class SetServo:
@@ -152,8 +159,9 @@ class State1(State):
         
     def __call__(self, flags:Flags, motorThread:MotorThread, setServo:SetServo, rover:Movement):
         if self.checkEntryCondition(flags):
-            print(f"Entered {self.NAME}")
+            #print(f"Entered {self.NAME}")
             self.action(flags, motorThread, setServo, rover)
+            flags.history.append(1)
             return flags, motorThread, setServo, rover
         return flags, motorThread, setServo, rover
 
@@ -186,8 +194,9 @@ class State2(State):
         
     def __call__(self, flags:Flags, motorThread:MotorThread, setServo:SetServo, rover:Movement):
         if self.checkEntryCondition(flags):
-            print(f"Entered {self.NAME}")
+            #print(f"Entered {self.NAME}")
             self.action(flags, motorThread, setServo, rover)
+            flags.history.append(2)
             return flags, motorThread, setServo, rover
         return flags, motorThread, setServo, rover
 
@@ -227,8 +236,9 @@ class State3(State):
         
     def __call__(self, flags:Flags, motorThread:MotorThread, setServo:SetServo, rover:Movement):
         if self.checkEntryCondition(flags):
-            print(f"Entered {self.NAME}")
+            #print(f"Entered {self.NAME}")
             self.action(flags, motorThread, setServo, rover)
+            flags.history.append(3)
             return flags, motorThread, setServo, rover
         return flags, motorThread, setServo, rover
 
@@ -255,7 +265,7 @@ class State3(State):
         else:
             new_flags.switch_updateCompass
             new_flags.switch_updateGPS
-            
+            new_flags.set_pastFlag_s3_history(new_flags.manualActions.signal_turnRight)
             self.angle_moved = 0
             return new_flags, new_motorThread, new_setServo, rover
 
@@ -268,8 +278,9 @@ class State4(State):
         
     def __call__(self, flags:Flags, motorThread:MotorThread, setServo:SetServo, rover:Movement):
         if self.checkEntryCondition(flags):
-            print(f"Entered {self.NAME}")
+            #print(f"Entered {self.NAME}")
             self.action(flags, motorThread, setServo, rover)
+            flags.history.append(4)
             return flags, motorThread, setServo, rover
         return flags, motorThread, setServo, rover
 
@@ -285,7 +296,7 @@ class State4(State):
         new_motorThread = motorThread
         
         
-        rover(VELOCITY, new_flags.manualActions.signal_turnRight*OMEGA)
+        rover(VELOCITY, new_flags.pastFlag_s3_history*OMEGA)
 
         new_flags.switch_updateGPS
         new_flags.continueMoving = True
@@ -299,8 +310,9 @@ class State5(State):
         
     def __call__(self, flags:Flags, motorThread:MotorThread, setServo:SetServo, rover:Movement):
         if self.checkEntryCondition(flags):
-            print(f"Entered {self.NAME}")
+            #print(f"Entered {self.NAME}")
             self.action(flags, motorThread, setServo, rover)
+            flags.history.append(5)
             return flags, motorThread, setServo, rover
         return flags, motorThread, setServo, rover
 
@@ -343,8 +355,9 @@ class State6(State):
         
     def __call__(self, flags:Flags, motorThread:MotorThread, setServo:SetServo, rover:Movement):
         if self.checkEntryCondition(flags):
-            print(f"Entered {self.NAME}")
+            #print(f"Entered {self.NAME}")
             self.action(flags, motorThread, setServo, rover)
+            flags.history.append(6)
             return flags, motorThread, setServo, rover
         return flags, motorThread, setServo, rover
 
@@ -372,8 +385,9 @@ class State7(State):
         
     def __call__(self, flags:Flags, motorThread:MotorThread, setServo:SetServo, rover:Movement):
         if self.checkEntryCondition(flags):
-            print(f"Entered {self.NAME}")
+            #print(f"Entered {self.NAME}")
             self.action(flags, motorThread, setServo, rover)
+            flags.history.append(7)
             return flags, motorThread, setServo, rover
         return flags, motorThread, setServo, rover
 
@@ -406,8 +420,9 @@ class State8(State):
         
     def __call__(self, flags:Flags, motorThread:MotorThread, setServo:SetServo, rover:Movement):
         if self.checkEntryCondition(flags):
-            print(f"Entered {self.NAME}")
+            #print(f"Entered {self.NAME}")
             self.action(flags, motorThread, setServo, rover)
+            flags.history.append(8)
             return flags, motorThread, setServo, rover
         return flags, motorThread, setServo, rover
 
@@ -436,8 +451,9 @@ class State9(State):
         
     def __call__(self, flags:Flags, motorThread:MotorThread, setServo:SetServo, rover:Movement):
         if self.checkEntryCondition(flags):
-            print(f"Entered {self.NAME}")
+            # print(f"Entered {self.NAME}")
             self.action(flags, motorThread, setServo, rover)
+            flags.history.append(9)
             return flags, motorThread, setServo, rover
         return flags, motorThread, setServo, rover
 
@@ -457,7 +473,7 @@ class State9(State):
     
 
 class StateManager:
-    def __init__(self):
+    def __init__(self, delta_t):
         
 
         state_1 = State1()
@@ -469,30 +485,40 @@ class StateManager:
         state_7 = State7()
         state_8 = State8()
         state_9 = State9()
+        
         self.states = [state_1, state_2, state_3, state_4, state_5, state_6, state_7, state_8, state_9]
+        self.rover = Movement(delta_t)
+        self.flags = Flags(False, False, True, False, False)
+        self.motorThread = MotorThread()
+        self.motorThread.flag = True
+        self.setServo = SetServo()
+        self.delta_t = delta_t
         
         
     def __call__(self, manualActions:List[ManualActions], time:int = 50):
 
-        self.rover = Movement()
-        self.flags = Flags(False, False, True, False, False)
-        self.flags.set_ManualActions(manualActions[0])
+        
 
-        self.motorThread = MotorThread()
-        self.motorThread.flag = True
-        self.setServo = SetServo()
-
-        for i in range(time):
+        
+        if not (time/self.delta_t).is_integer():
+            raise ValueError("Error in total time and delta_t combination. total time should be divisible by delta_t")
+        else:
+            total_t = int(time/self.delta_t)
+        for i in range(total_t):
+            self.flags.set_ManualActions(manualActions[i])
             self.flags, self.motorThread, self.setServo, self.rover = self.checkNextState(self.flags, self.motorThread, self.setServo, self.rover)
+            
         return self.flags, self.motorThread, self.setServo, self.rover
     
-    def checkNextState(self, flags, motorThread, setServo, rover):
+    def checkNextState(self, flags:Flags, motorThread:MotorThread, setServo:SetServo, rover:Movement):
         
 
         for s in self.states:
-            new_flags, new_motorThread, new_setServo, new_rover = s(flags, motorThread, setServo, rover)
-            flags, motorThread, setServo, rover = new_flags, new_motorThread, new_setServo, new_rover
-        return flags, motorThread, setServo, rover
+            if s.checkEntryCondition(flags):
+                new_flags, new_motorThread, new_setServo, new_rover = s(flags, motorThread, setServo, rover)
+                flags, motorThread, setServo, rover = new_flags, new_motorThread, new_setServo, new_rover
+                
+                return flags, motorThread, setServo, rover
 
 
 def generateManualActions(signal_55, signal_66, signal_turnRight):
@@ -519,40 +545,44 @@ RoverHAResultT = ExtraResult[RoverHADataT, RoverHADataT]
 class RoverHAModel(Model[RoverHAResultT, None]):
 
     def __init__(self) -> None:
-        self.sampling_step = 1
+        self.delta_t = 0.1
 
     def simulate(self, inputs:ModelInputs, intrvl: Interval) -> RoverHAResultT:
-        n_times = (intrvl.length // self.sampling_step)
+        man = StateManager(self.delta_t )
+        
+        n_times = (intrvl.length // self.delta_t )+2
         signal_times = np.linspace(intrvl.lower, intrvl.upper, int(n_times))
         inputs_bounds = [i*intrvl.length for i in inputs.static]
         signal_55 = np.zeros_like(signal_times)
-        for i, t in enumerate(np.sort(inputs_bounds[:4])):
-            if i % 2 == 0:
-                signal_55[signal_times >= t] = 1  # Set signal to 1 at even indices (rise)
-            else:
-                signal_55[signal_times >= t] = 0  # Set signal to 0 at odd indices (fall)
+        # for i, t in enumerate(np.sort(inputs_bounds[:4])):
+        #     if i % 2 == 0:
+        #         signal_55[signal_times >= t] = 1  # Set signal to 1 at even indices (rise)
+        #     else:
+        #         signal_55[signal_times >= t] = 0  # Set signal to 0 at odd indices (fall)
 
         signal_66 = np.zeros_like(signal_times)
-        for i, t in enumerate(np.sort(inputs_bounds[4:8])):
-            if i % 2 == 0:
-                signal_66[signal_times >= t] = 1  # Set signal to 1 at even indices (rise)
-            else:
-                signal_66[signal_times >= t] = 0  # Set signal to 0 at odd indices (fall)
+        # for i, t in enumerate(np.sort(inputs_bounds[4:8])):
+        #     if i % 2 == 0:
+        #         signal_66[signal_times >= t] = 1  # Set signal to 1 at even indices (rise)
+        #     else:
+        #         signal_66[signal_times >= t] = 0  # Set signal to 0 at odd indices (fall)
 
         signal_tr = np.zeros_like(signal_times)-1
-        for i, t in enumerate(np.sort(inputs_bounds[8:])):
+        for i, t in enumerate(np.sort(inputs_bounds)):
             if i % 2 == 0:
                 signal_tr[signal_times >= t] = 1  # Set signal to 1 at even indices (rise)
             else:
                 signal_tr[signal_times >= t] = -1  # Set signal to 0 at odd indices (fall)
 
 
-        man = StateManager()
-        manualActions:List[ManualActions] = generateManualActions(signal_55=signal_55, signal_66=signal_66, signal_turnRight=signal_tr)
-        flags, motorThread, setServo, rover = man(manualActions, T)
-
         
-        data_array = np.array([rover.history_x, rover.history_y, rover.history_theta])
+        manualActions:List[ManualActions] = generateManualActions(signal_55=signal_55, signal_66=signal_66, signal_turnRight=signal_tr)
+        flags, motorThread, setServo, rover = man(manualActions, intrvl.length)
+
+        r_theta = copy(rover.history_theta)
+        r_theta.append(r_theta[-1])
+        diff_theta = np.diff(r_theta).tolist()
+        data_array = np.array([rover.history_x, rover.history_y, rover.history_theta, flags.history, signal_tr, diff_theta])
         timestamps_list = np.array(rover.history_t).flatten()
         
         trace = Trace(timestamps_list, data_array.T)
@@ -564,16 +594,14 @@ class RoverHAModel(Model[RoverHAResultT, None]):
 
 if __name__ == "__main__":
     
-    VELOCITY = 1
-    OMEGA = np.pi/4
+    VELOCITY = 0.1
+    OMEGA = np.pi/36
     
-    rng = np.random.default_rng(12346)
-    T = 50 
+    rng = np.random.default_rng(12341)
+    T = 100
 
 
     init_conditions = [
-                        [0,1],[0,1],[0,1],[0,1], 
-                        [0,1],[0,1],[0,1],[0,1],
                         [0,1],[0,1],[0,1],[0,1]
                     ]
     options = Options(runs=1, iterations=1, interval=(0, T), signals = [], static_parameters=init_conditions)
@@ -582,18 +610,47 @@ if __name__ == "__main__":
     # signal_bounds = sum((signal.control_points for signal in signals), ())
     bounds = options.static_parameters
     rng = np.random.default_rng(12345)
-    sample = Sample([rng.uniform(bound.lower, bound.upper) for bound in bounds])
+    # sample = Sample([rng.uniform(bound.lower, bound.upper) for bound in bounds])
+    sample = Sample([5.1/100,10/100,20/100,30/100])
 
     model = RoverHAModel()
     res = simulate_model(model, options, sample)
+    # spec_1 = f"((f <= -1) and (state <= 4.5) and (state >= 2.5)) -> (F[0,{T/4}] (delta < 0))"
+    # spec_2 = f"((f >= 1) and (state <= 4.5) and (state >= 2.5)) -> (F[0,{T/4}] (delta > 0))"
+    spec_1 = f"((f == -1) and (state > 4.5)) -> (F[0,{T/4}] (delta < -1e-3 or delta > 1e-3))"
+    spec_2 = f"((f ==  1) and (state > 4.5)) -> (F[0,{T/4}] (delta < -1e-3 or delta > 1e-3))"
+    phi = f"G({spec_1} or {spec_2})"
+    req = RTAMTDense(phi, {"f":4, "state":3, "delta": 5})
 
+    x = req.evaluate(res.trace.states, res.trace.times)
+    print(x)
     data = np.array(res.trace.states)
-    plt.figure(figsize=(8, 8))
-    plt.plot(data[:,0], data[:,1], "-")
-    plt.title('Trajectory of the Point')
-    plt.xlabel('X position (m)')
-    plt.ylabel('Y position (m)')
-    plt.legend()
+
+    fig, axs = plt.subplots(2,2, figsize=(12, 12))  # 3 rows, 1 column
+
+    axs[0,0].plot(data[:,0], data[:,1], "-")
+    # axs[0].set_title('Trajectory of the Point')
+    axs[0,0].set_xlabel('X position (m)')
+    axs[0,0].set_ylabel('Y position (m)')
+
+    axs[0,1].plot(res.trace.times, data[:,2], "-")
+    # axs[1].set_title('Angle')
+    axs[0,1].set_xlabel('Time')
+    axs[0,1].set_ylabel('Theta')
+
+    axs[1,1].plot(res.trace.times, data[:,4], "-")
+    axs[1,1].plot(res.trace.times, data[:,3], "-")
+    # axs[1,1].set_ylim([-1.2,1.2])
+    # axs[2].set_title('Angle')
+    axs[1,1].set_xlabel('Time')
+    axs[1,1].set_ylabel('Turn Right')
+
+    axs[1,0].plot(res.trace.times, data[:,3], "-")
+    axs[1,0].set_ylim([-1,10])
+    # axs[3].set_title('States')
+    axs[1,0].set_xlabel('Time')
+    axs[1,0].set_ylabel('States')
+
     plt.grid(True)
-    plt.axis('equal')
+    # plt.axis('equal')
     plt.savefig("test_ozmen.png")
