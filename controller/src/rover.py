4c5e5e6a4881110db255ec1c9dca1143f97d3879
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-01-22 11:03:52 -0500

    Add sentinel value to rover properties to ensure they are set when switched
    If you set the velocity of the rover, then the angular velocity, then
    the velocity to the initial value, the rover would not stop turning
    because the velocity was not reset when the angular velocity was set.
    Using this sentinel value will allow the rover to know when its modes
    are switched so it can make sure to send the correct commands.

--- controller/src/rover.py
+++ controller/src/rover.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass, field
 from logging import Logger, NullHandler, getLogger
 from math import pi
 from threading import Event, Lock
+from time import sleep
 
 from gz.transport13 import Node, Publisher, SubscribeOptions
 from gz.math7 import Quaterniond
@@ -16,7 +17,7 @@ from controller import attacks, automaton
 
 
 def _pose_logger() -> Logger:
-    logger = getLogger("rover.PoseHandler")
+    logger = getLogger("rover.pose")
     logger.addHandler(NullHandler())
 
     return logger
@@ -82,14 +83,22 @@ class PoseHandler:
         self._ready.wait()
 
 
+def _rover_logger() -> Logger:
+    logger = getLogger("rover")
+    logger.addHandler(NullHandler())
+
+    return logger
+
+
 @dataclass()
 class Rover(automaton.Model):
     _node: Node = field()
     _motors: Publisher = field()
     _pose: PoseHandler = field()
     _magnet: attacks.Magnet = field()
-    _velocity: float = field(default=0.0, init=False)
-    _omega: float = field(default=0.0, init=False)
+    _velocity: float | None = field(default=None, init=False)
+    _omega: float | None = field(default=None, init=False)
+    _logger: Logger = field(default_factory=_rover_logger, init=False)
 
     @property
     def clock(self) -> float:
@@ -109,31 +118,35 @@ class Rover(automaton.Model):
 
     @property
     def omega(self) -> float:
-        return self._omega
+        return self._omega or 0.0
 
     @omega.setter
     def omega(self, target: float):
-        if target != self._omega:
+        if self._omega is None or target != self._omega:
             msg = Actuators()
             msg.velocity.append(target)
             msg.velocity.append(target)
 
             self._motors.publish(msg)
             self._omega = target
+            self._velocity = None
+            self._logger.info(f"Setting angular velocity to {target}")
 
     @property
     def velocity(self) -> float:
-        return self._velocity
+        return self._velocity or 0.0
 
     @velocity.setter
     def velocity(self, target: float):
-        if target != self._velocity:
+        if self._velocity is None or target != self._velocity:
             msg = Actuators()
             msg.velocity.append(-target)
             msg.velocity.append(target)
 
             self._motors.publish(msg)
             self._velocity = target
+            self._omega = None
+            self._logger.info(f"Setting velocity to {target}")
 
     def wait(self):
         self._pose.wait()
@@ -156,7 +169,10 @@ def spawn(world: str, *, magnet: attacks.Magnet, name: str = "r1_rover") -> Rove
     msg.sdf_filename = "r1_rover/model.sdf"
     msg.name = name
     msg.allow_renaming = False
-    res, rep = node.request(f"/world/{world}/create", msg, EntityFactory, Boolean, timeout=1000)
+    res, rep = node.request(f"/world/{world}/create", msg, EntityFactory, Boolean, timeout=5000)
+
+    logger.debug(f"Response: {res}")
+    logger.debug(f"Reply: {rep}")
 
     if not res:
         raise TransportError("Failed to send Gazebo message for rover creation")
@@ -164,19 +180,20 @@ def spawn(world: str, *, magnet: attacks.Magnet, name: str = "r1_rover") -> Rove
     if not rep.data:
         raise RoverError("Could not create rover Gazebo model")
 
-    logger.debug("Created rover model in gazebo world")
-    motors = node.advertise(f"/model/{name}/command/motor_speed", Actuators)
-
-    if not motors.valid():
-        raise TransportError("Could not register publisher for motor control")
-
-    logger.debug("Created motor topic publisher")
-
     pose = PoseHandler(name)
     pose_options = SubscribeOptions()
     pose_options.msgs_per_sec = 10
 
     if not node.subscribe(Pose_V, f"/world/{world}/pose/info", pose, pose_options):
         raise TransportError()
+
+    logger.info("Subscribed to pose topic.")
+    motors = node.advertise(f"/model/{name}/command/motor_speed", Actuators)
+
+    if not motors.valid():
+        raise TransportError("Could not register publisher for motor control")
+
+    logger.info("Initialized motor topic publisher.")
+    logger.info(f"Creating rover model {name} in gazebo world {world}...")
 
     return Rover(node, motors, pose, magnet)
