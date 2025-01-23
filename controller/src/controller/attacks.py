f5eb32146de6b4bf22704a1e6333d41ecc47110e
Michael Link <Michael.J.Link@hii.com>
2025-01-23 12:25:55 -0500

    EMI-integration added to attacks
    -EMF attack integration

--- controller/src/controller/attacks.py
+++ controller/src/controller/attacks.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
 from typing import Protocol
-
+from .automaton import Model
+import numpy as np
+from scipy.spatial.transform import Rotation
 
 class Magnet(Protocol):
     def offset(self, time: float) -> float:
@@ -14,7 +16,24 @@ class StationaryMagnet(Magnet):
 
     def offset(self, time: float) -> float:
         return self.magnitude
+    
+class EMFMagnet(Magnet):
+    def __init__(self, magnitude: float, position: tuple[float,float,float]):
+        self.pos = position
+        self.magnitude = magnitude
 
+    def offset(self, time: float, model: Model) -> float:
+        r = model.position - self.pos
+        distance = np.linalg.norm(r)
+        unit_r = r/distance
+        b_scalar = self.magnitude/(4.0*np.pi*np.power(distance,3.0)) 
+        b_vector = unit_r*np.dot(np.array([0.0,0.0,1.0]),unit_r)*3.0 - np.array([0.0,0.0,1.0])
+        b = b_scalar * b_vector
+        b = unit_r*np.linalg.norm(b)
+        b += model.worldField # np.array([0.224902,0.0,0.428])
+        b = Rotation.from_euler(0.0,0.0,model.heading).inv().apply(b)
+        newHeading = -np.rad2deg(np.arctan2(b[1],b[0]))
+        return (newHeading - model.heading)
 
 class SpeedController:
     def speed(self, time: float) -> float:
