15e0a3da447a34f71a5533a09d93621167ec42d8
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-04-24 09:46:04 -0700

    Allow users to plot state machine evolution over time

--- src/plots.py
+++ src/plots.py
@@ -7,6 +7,8 @@ from matplotlib import pyplot as plt
 from matplotlib import patches as patches
 from staliro import Trace
 
+from controller.automaton import State
+
 
 @dataclass()
 class Plot:
@@ -15,11 +17,33 @@ class Plot:
     color: Literal["r", "g", "b", "k"] = "k"
 
 
-def plot(*plots: Plot):
-    _, ax = plt.subplots()
+@dataclass()
+class StatePlot:
+    trajectory: Trace[State]
+
+
+def plot(*plots: Plot, states: StatePlot | None = None):
+    plt.rc("font", size=18)
+
+    if states:
+        _, axs = plt.subplots(1, 2)
+        axs[1].set_title("Discrete State Trajectory")
+        axs[1].set_xlabel("Time (s)")
+        axs[1].set_ylabel("State")
+        axs[1].plot(
+            list(states.trajectory.times),
+            [int(state) for state in states.trajectory.states]
+        )
+
+        ax = axs[0]
+    else:
+        _, ax = plt.subplots(1, 2)
+
     ax.set_title("Trajectory")
     # ax.set_xlim(left=0, right=16)
     ax.set_ylim(bottom=-2, top=10)
+    ax.set_xlabel("X (m)")
+    ax.set_ylabel("Y (m)")
     ax.add_patch(patches.Rectangle((0, 0), 8, 8, linewidth=1, edgecolor="r", fill=False))
     magnets = [plot.magnet for plot in plots if plot.magnet is not None]
 
